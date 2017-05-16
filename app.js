6507976a71aa841157f0964b7c431c4b613b2bda
Mikhail.Chirich <chirichok98@gmail.com>
2017-05-16 11:32:32 +0300

    meduza totaly deleted

--- app.js
+++ app.js
@@ -2,14 +2,12 @@ const express = require('express');
 const mongo = require('./db');
 const articles = require('./routers/articles');
 const users = require('./routers/users');
-const meduza = require('./routers/meduza');
 
 const app = express();
 
 app.use(express.static(`${__dirname}/public`));
 app.use('/', users);
 app.use('/articles', articles);
-app.use('/meduza', meduza);
 
 const url = 'mongodb://localhost:27017/chirich';
 
