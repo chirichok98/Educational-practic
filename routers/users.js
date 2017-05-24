920c334186cf850fa4c6a42b0511afb653fc9c5d
Mikhail.Chirich <chirichok98@gmail.com>
2017-05-24 21:41:14 +0300

    bug with mongo atlas fixed

--- routers/users.js
+++ routers/users.js
@@ -6,12 +6,14 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const router = require('express').Router();
 
+const url = 'mongodb://server:6ihfqbl3KZUVSze1@cluster0-shard-00-00-guq8k.mongodb.net:27017,cluster0-shard-00-01-guq8k.mongodb.net:27017,cluster0-shard-00-02-guq8k.mongodb.net:27017/Portal?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
+
 const sessionOptions = {
   secret: 'MY_SECRET',
   saveUninitialized: false,
   resave: false,
   store: new MongoStore({
-    url: 'mongodb://localhost:27017/chirich',
+    url,
     touchAfter: 24 * 3600
   })
 };
